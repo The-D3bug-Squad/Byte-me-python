3dccaa7972f68ddb82154392203a1a6cde8cb9e8
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-12-18 20:33:23 +0200

    ...

--- functions_to_test.py
+++ functions_to_test.py
@@ -1,28 +1,63 @@
 # Placeholder functions for Python basics, to be implemented later
 
 def add_numbers(a, b):
-    pass
+    return a + b
 
 def find_maximum(a, b, c):
-    pass
+    return max(a, b, c)
 
 def is_palindrome(string):
-    pass
+    if type(string) == type("stirng"):
+        if string == string[::-1]:
+            return True
+        else:
+            return False
+    else:
+        raise TypeError("Invalid input!")
 
 def count_word_occurrences(text, word):
-    pass
+    if type(text) == type(word):
+        text = text.upper()
+        word = word.upper()
+        count = text.count(word)
+        return count
+    else:
+        raise TypeError("Invalid type entered")
 
 def read_file_lines(filepath):
-    pass
+    try:
+        with open(filepath, "r") as f:
+            file = f.readlines()
+    except FileNotFoundError as e:
+        print(e)
+
+
 
 def factorial(n):
-    pass
+    answer = 1
+    for i in range(1, n + 1):
+        if i != n:
+            print(i)
+            answer *= i * i + 1
+        else:
+            answer *= n
 
 def is_prime(n):
-    pass
-
+    try:
+        if type(n) == type(0):
+            outcome = is_prime(n)
+            return outcome
+    except TypeError as e:
+        print(e)
+    except RecursionError as e:
+        print(e)
+    
 def sort_numbers(numbers):
-    pass
+    try:
+        if type(numbers) == type([]):
+            return sorted(numbers)
+    except TypeError:
+        print("invalid type")
 
 def factorial(n):
     pass
@@ -52,7 +87,13 @@ def tower_of_hanoi(n, source, auxiliary, target):
 
 class Person:
     def __init__(self, name, age):
-        pass
+        self.name = name
+        self.age = age
+
+        if type(name) != type("string"):
+            raise TypeError
+        if type(age) != type(0):
+            raise TypeError
 
 
 if __name__ == "__main__":
